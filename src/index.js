a7bd1f63a3bd082fa2b7df7555e98f0d24a30135
Michael Malka <michael@talkingagents.com>
2025-10-27 23:30:26 +0200

    Cors update for agentplanner.io

--- src/index.js
+++ src/index.js
@@ -38,7 +38,14 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:3001',
+    'https://agentplanner.io',
+    'https://www.agentplanner.io'
+  ],
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
